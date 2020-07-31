eeb65ed45ab0acc38ff28a0a2efa43d82afd6b22
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-07-31 11:55:49 -0400

    Renamed project from IIIS to IIIF

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "footlight_iiis_production"
+  # config.active_job.queue_name_prefix = "footlight_iiif_production"
 
   config.action_mailer.perform_caching = false
 
